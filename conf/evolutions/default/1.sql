66792fb5954f9f4c334d4db978bb39bc2d83d1d4
Guernion Sylvain <gsylvain35@gmail.com>
2012-10-14 13:50:51 +0200

    conf cloudbees

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table comment (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   author_id                 bigint,
   talk_id                   bigint,
   comment                   varchar(140),
@@ -12,7 +12,7 @@ create table comment (
 ;
 
 create table lien (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   user_id                   bigint not null,
   label                     varchar(50),
   url                       varchar(200),
@@ -20,7 +20,7 @@ create table lien (
 ;
 
 create table talk (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   title                     varchar(50),
   description               varchar(2000),
   speaker_id                bigint,
@@ -32,39 +32,29 @@ create table token (
   token                     varchar(255) not null,
   user_id                   bigint,
   type                      varchar(8),
-  date_creation             timestamp,
+  date_creation             datetime,
   email                     varchar(255),
   constraint ck_token_type check (type in ('password','email')),
   constraint pk_token primary key (token))
 ;
 
 create table user (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   email                     varchar(255),
   fullname                  varchar(255),
   confirmation_token        varchar(255),
   password_hash             varchar(255),
-  date_creation             timestamp,
-  validated                 boolean,
-  admin                     boolean,
-  notif_on_my_talk          boolean,
-  notif_admin_on_all_talk   boolean,
-  notif_admin_on_talk_with_comment boolean,
+  date_creation             datetime,
+  validated                 tinyint(1) default 0,
+  admin                     tinyint(1) default 0,
+  notif_on_my_talk          tinyint(1) default 0,
+  notif_admin_on_all_talk   tinyint(1) default 0,
+  notif_admin_on_talk_with_comment tinyint(1) default 0,
   description               varchar(2000),
   constraint uq_user_email unique (email),
   constraint uq_user_fullname unique (fullname),
   constraint pk_user primary key (id))
 ;
-
-create sequence comment_seq;
-
-create sequence lien_seq;
-
-create sequence talk_seq;
-
-create sequence token_seq;
-
-create sequence user_seq;
 
 alter table comment add constraint fk_comment_author_1 foreign key (author_id) references user (id) on delete restrict on update restrict;
 create index ix_comment_author_1 on comment (author_id);
@@ -79,27 +69,17 @@ create index ix_talk_speaker_4 on talk (speaker_id);
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists comment;
+drop table comment;
 
-drop table if exists lien;
+drop table lien;
 
-drop table if exists talk;
+drop table talk;
 
-drop table if exists token;
+drop table token;
 
-drop table if exists user;
+drop table user;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists comment_seq;
-
-drop sequence if exists lien_seq;
-
-drop sequence if exists talk_seq;
-
-drop sequence if exists token_seq;
-
-drop sequence if exists user_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       "com.typesafe" %% "play-plugins-mailer" % "2.0.2",
       "org.mindrot" % "jbcrypt" % "0.3m",
-      "mysql" % "mysql-connector-java" % "5.1.18"
+      "mysql" % "mysql-connector-java" % "5.1.21"
     )
 
     lazy val s = Defaults.defaultSettings ++ Seq(generateAPIDocsTask)
