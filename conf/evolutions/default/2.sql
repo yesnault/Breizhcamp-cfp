b1b2844e127e16fdee58246ec4a33776ed0b1baa
Guernion Sylvain <gsylvain35@gmail.com>
2012-11-03 01:42:11 +0100

    scripts sql evolution

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,8 +1,11 @@
 
 # --- !Ups
+alter table talk add status_talk integer;
+alter table talk add constraint ck_talk_status_talk check (status_talk in (0,1,2));
+
 
 create table tag (
-id                        bigint not null,
+id                        bigint auto_increment not null,
 nom                       varchar(255),
 constraint uq_tag_nom unique (nom),
 constraint pk_tag primary key (id))
@@ -14,8 +17,6 @@ talk_id                        bigint not null,
 constraint pk_tag_talk primary key (tag_id, talk_id))
 ;
 
-
-create sequence tag_seq;
 
 alter table tag_talk add constraint fk_tag_talk_tag_01 foreign key (tag_id) references tag (id) on delete restrict on update restrict;
 
@@ -35,4 +36,5 @@ drop table if exists tag;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
-drop sequence if exists tag_seq;
+alter table talk delete status_talk;
+alter table talk delete constraint ck_talk_status_talk;
