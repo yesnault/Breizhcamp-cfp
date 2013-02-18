14fd874fa3f9b0acd3ba78e9faf7157df3458185
Laurent HUET <laurent.huet@gmail.com>
2013-02-18 22:11:00 +0100

    script d'initialisation de base

---
+++ init.sql
@@ -0,0 +1,303 @@
+-- MySQL dump 10.13  Distrib 5.5.29, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: breizhcamp
+-- ------------------------------------------------------
+-- Server version	5.5.29-0ubuntu0.12.04.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `author_id` bigint(20) DEFAULT NULL,
+  `talk_id` bigint(20) DEFAULT NULL,
+  `comment` varchar(140) DEFAULT NULL,
+  `clos` tinyint(1) DEFAULT '0',
+  `private_comment` tinyint(1) DEFAULT '0',
+  `question_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_comment_author_1` (`author_id`),
+  KEY `ix_comment_talk_2` (`talk_id`),
+  KEY `ix_comment_question_3` (`question_id`),
+  CONSTRAINT `fk_comment_author_1` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_comment_question_3` FOREIGN KEY (`question_id`) REFERENCES `comment` (`id`),
+  CONSTRAINT `fk_comment_talk_2` FOREIGN KEY (`talk_id`) REFERENCES `talk` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `credentials`
+--
+
+DROP TABLE IF EXISTS `credentials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `credentials` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `ext_user_id` varchar(255) DEFAULT NULL,
+  `provider_id` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `o_auth1token` varchar(255) DEFAULT NULL,
+  `o_auth1secret` varchar(255) DEFAULT NULL,
+  `o_auth2access_token` varchar(255) DEFAULT NULL,
+  `o_auth2token_type` varchar(255) DEFAULT NULL,
+  `o_auth2expires_in` int(11) DEFAULT NULL,
+  `o_auth2refresh_token` varchar(255) DEFAULT NULL,
+  `password_hasher` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `password_salt` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_credentials_user_4` (`user_id`),
+  CONSTRAINT `fk_credentials_user_4` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `creneau`
+--
+
+DROP TABLE IF EXISTS `creneau`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `creneau` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) DEFAULT NULL,
+  `duree_minutes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_creneau_libelle` (`libelle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `creneau_talk`
+--
+
+DROP TABLE IF EXISTS `creneau_talk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `creneau_talk` (
+  `creneau_id` bigint(20) NOT NULL,
+  `talk_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`creneau_id`,`talk_id`),
+  KEY `fk_creneau_talk_talk_02` (`talk_id`),
+  CONSTRAINT `fk_creneau_talk_creneau_01` FOREIGN KEY (`creneau_id`) REFERENCES `creneau` (`id`),
+  CONSTRAINT `fk_creneau_talk_talk_02` FOREIGN KEY (`talk_id`) REFERENCES `talk` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dynamic_field`
+--
+
+DROP TABLE IF EXISTS `dynamic_field`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dynamic_field` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_dynamic_field_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dynamic_field_value`
+--
+
+DROP TABLE IF EXISTS `dynamic_field_value`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dynamic_field_value` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `value` varchar(255) DEFAULT NULL,
+  `dynamic_field_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_dynamic_field_value_dynamic_5` (`dynamic_field_id`),
+  KEY `ix_dynamic_field_value_user_6` (`user_id`),
+  CONSTRAINT `fk_dynamic_field_value_dynamic_5` FOREIGN KEY (`dynamic_field_id`) REFERENCES `dynamic_field` (`id`),
+  CONSTRAINT `fk_dynamic_field_value_user_6` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lien`
+--
+
+DROP TABLE IF EXISTS `lien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lien` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_lien_user_7` (`user_id`),
+  CONSTRAINT `fk_lien_user_7` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `play_evolutions`
+--
+
+DROP TABLE IF EXISTS `play_evolutions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `play_evolutions` (
+  `id` int(11) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  `applied_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `apply_script` text,
+  `revert_script` text,
+  `state` varchar(255) DEFAULT NULL,
+  `last_problem` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_tag_nom` (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tag_talk`
+--
+
+DROP TABLE IF EXISTS `tag_talk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_talk` (
+  `tag_id` bigint(20) NOT NULL,
+  `talk_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`tag_id`,`talk_id`),
+  KEY `fk_tag_talk_talk_02` (`talk_id`),
+  CONSTRAINT `fk_tag_talk_tag_01` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`),
+  CONSTRAINT `fk_tag_talk_talk_02` FOREIGN KEY (`talk_id`) REFERENCES `talk` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `talk`
+--
+
+DROP TABLE IF EXISTS `talk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `talk` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `speaker_id` bigint(20) DEFAULT NULL,
+  `status_talk` varchar(1) DEFAULT NULL,
+  `duree_preferee_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_talk_title` (`title`),
+  KEY `ix_talk_speaker_8` (`speaker_id`),
+  KEY `ix_talk_dureePreferee_9` (`duree_preferee_id`),
+  CONSTRAINT `fk_talk_dureePreferee_9` FOREIGN KEY (`duree_preferee_id`) REFERENCES `creneau` (`id`),
+  CONSTRAINT `fk_talk_speaker_8` FOREIGN KEY (`speaker_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `authentication_method` varchar(255) DEFAULT NULL,
+  `credentials_id` bigint(20) DEFAULT NULL,
+  `date_creation` datetime DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT '0',
+  `notif_on_my_talk` tinyint(1) DEFAULT '0',
+  `notif_admin_on_all_talk` tinyint(1) DEFAULT '0',
+  `notif_admin_on_talk_with_comment` tinyint(1) DEFAULT '0',
+  `adresse_mac` varchar(255) DEFAULT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_user_email` (`email`),
+  KEY `ix_user_credentials_10` (`credentials_id`),
+  CONSTRAINT `fk_user_credentials_10` FOREIGN KEY (`credentials_id`) REFERENCES `credentials` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vote`
+--
+
+DROP TABLE IF EXISTS `vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vote` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `talk_id` bigint(20) DEFAULT NULL,
+  `note` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_vote_user_11` (`user_id`),
+  KEY `ix_vote_talk_12` (`talk_id`),
+  CONSTRAINT `fk_vote_talk_12` FOREIGN KEY (`talk_id`) REFERENCES `talk` (`id`),
+  CONSTRAINT `fk_vote_user_11` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vote_status`
+--
+
+DROP TABLE IF EXISTS `vote_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vote_status` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-18 11:06:02
