a34fb9dcfbabc3a56ce8d8986a2abdb8f058cb0c
Laurent HUET <laurent.huet@gmail.com>
2013-02-22 10:51:58 +0100

    Correction timeout avec intercepteur HTTP angular

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -36,6 +36,29 @@ App.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/reset/:token', {templateUrl: 'assets/pages/partials/confirmresetpassword.html', controller: ConfirmResetPasswordController});
     $routeProvider.when('/confirm/:token', {templateUrl: 'assets/pages/partials/confirmsignup.html', controller: ConfirmSignupController});
     $routeProvider.when('/email/:token', {templateUrl: 'assets/pages/partials/settings/confirmemail.html', controller: ConfirmEmailController});
-    $routeProvider.otherwise({redirectTo: '/dashboard'});
+    $routeProvider.otherwise({redirectTo: '/dashboard'}); 
   }]);
+
+// Intercepteur HTTP pour gérer le timeout de session
+// Inspiré de http://www.espeo.pl/2012/02/26/authentication-in-angularjs-application
+App.config(function($httpProvider) {
+      
+    var authentInterceptor = ['$rootScope', '$q', '$log', function (scope, $q, $log) {
+        function success(response) {
+            return response;
+        }
+        function error(response) {
+            var status = response.status;
+            if (status == 401) {
+                $log.info('401 -> event:unauthorized');
+                scope.$broadcast('event:unauthorized');
+            }
+            return $q.reject(response);
+        }
+        return function(promise) {
+            return promise.then(success, error);
+        };
+    }];
+    $httpProvider.responseInterceptors.push(authentInterceptor);
+});
 
--- public/javascripts/angular/directives.js
+++ public/javascripts/angular/directives.js
@@ -3,25 +3,34 @@
 /* Directives */
 
 angular.module('breizhCampCFP.directives', [])
-    .directive('markdownpreview', function($log) {
-        // return the directive link function. (compile function not needed)
-        return function(scope, element, attrs) {
-            var content; // contenu markdown
+        .directive('markdownpreview', function($log) {
+    // return the directive link function. (compile function not needed)
+    return function(scope, element, attrs) {
+        var content; // contenu markdown
 
-            // used to update the UI
-            function updateContent() {
-                if (content !== undefined && content != null) {
-                    element.html(scope.converter.makeHtml(content));
-                } else {
-                    element.text('');
-                }
+        // used to update the UI
+        function updateContent() {
+            if (content != null) { // Si not undefined ou not null
+                element.html(scope.converter.makeHtml(content));
+            } else {
+                element.text('');
             }
-
-            // watch the expression, and update the UI on change.
-            scope.$watch(attrs.markdownpreview, function(value) {
-                content = value;
-                updateContent();
-            });
         }
-    });
+
+        // watch the expression, and update the UI on change.
+        scope.$watch(attrs.markdownpreview, function(value) {
+            content = value;
+            updateContent();
+        });
+    };
+})
+        .directive('sessionhandler', function($location, $log) {
+    // Directive pour gérer la fin de session
+    // Evenement envoyé par un intercepteur HTTP
+    return function(scope, element, attrs) {
+        scope.$on('event:unauthorized', function() {
+            $location.path('/login');
+        });
+    };
+});
 
