a8417214285c6b11d1ef7d07b00bd570d822f7ca
Guernion Sylvain <gsylvain35@gmail.com>
2013-04-13 11:32:39 +0200

    Modal dans la liste des talks, pour voir le détails et donner une note au talk

--- public/javascripts/angular/directives.js
+++ public/javascripts/angular/directives.js
@@ -30,21 +30,62 @@ directives.directive('star', function factory() {
         template: '<div id="star"> </div>',
         restrict: 'E',
         replace: true,
+        require: 'ngModel',
         transclude: true,
-        link: function postLink(scope, iElement, iAttrs) {
+        link: function postLink(scope, iElement, iAttr,ngModel) {
             $.fn.raty.defaults.path = '/assets/img/';
             $('#star').raty({
                 click: function(score, evt) {
-                    scope.talk.note = score;
+                    scope.$parent.talk = ngModel.$modelValue;
+                    scope.$parent.talk.note = score;
                     scope.$digest();
                 }
             });
 
-            scope.$watch('talk.note', function(value) {
+            scope.$watch(function () {
+                return ngModel.$modelValue.note;
+            }, function(value) {
                 $('#star').raty('score', value != undefined ? value : 1);
             });
+
         }
 
     };
     return directiveDefinitionObject;
 });
+
+directives.directive('stars', function factory() {
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        replace: true,
+        require: 'ngModel',
+        scope: {
+            talkid: '@'
+        },
+        transclude: true,
+        template: '<div id="star{{talkid}}" > </div>',
+        link: function postLink(scope, iElement, iAttr,ngModel) {
+            $.fn.raty.defaults.path = '/assets/img/';
+            $('#star'+scope.talkid).raty({
+                click: function(score, evt) {
+                    scope.$parent.talkModal = ngModel.$modelValue;
+                    scope.$parent.talkModal.note = score;
+                    scope.$digest();
+                }
+            });
+
+            scope.$watch(function () {
+                return ngModel.$modelValue!= undefined ?  ngModel.$modelValue.note:1;
+            }, function(value) {
+                console.debug('#star'+scope.talkid+'= '+value);
+                $('#star'+scope.talkid).raty('score',  value);
+            });
+
+        }
+
+    };
+    return directiveDefinitionObject;
+});
+
+
+
