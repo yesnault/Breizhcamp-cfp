d43e57c1e6aa6ca3f0adae3011ffa70fc68b4046
Guernion Sylvain <gsylvain35@gmail.com>
2014-01-21 00:59:14 +0100

    clean

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -10,15 +10,19 @@ App.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/login', {templateUrl: '/assets/pages/partials/login.html', controller: LoginController});
     $routeProvider.when('/signup', {templateUrl: '/assets/pages/partials/signup.html'});
     $routeProvider.when('/dashboard', {templateUrl: '/assets/pages/partials/dashboard.html', controller: DashboardController});
-    $routeProvider.when('/submitproposal/new', {templateUrl: '/assets/pages/partials/submitproposal.html', controller: NewProposalController});
-    $routeProvider.when('/editproposal/:proposalId', {templateUrl: '/assets/pages/partials/submitproposal.html', controller: EditProposalController});
-    $routeProvider.when('/manageproposal', {templateUrl: '/assets/pages/partials/manageproposals.html', controller: ManageProposalController});
+
     $routeProvider.when('/admin/users', {templateUrl: '/assets/pages/partials/admin/users.html', controller: ManageUsersController});
-    $routeProvider.when('/admin/proposals/list', {templateUrl: '/assets/pages/partials/admin/listproposals.html', controller: ListProposalsController});
+
     $routeProvider.when('/admin/vote', {templateUrl: 'assets/pages/partials/admin/vote.html', controller: VoteController});
     $routeProvider.when('/admin/formats', {templateUrl: 'assets/pages/partials/admin/formats.html', controller: CreneauxController});
     $routeProvider.when('/admin/format/new', {templateUrl: 'assets/pages/partials/admin/submitcreneau.html', controller: NewCreneauController});
     $routeProvider.when('/admin/format/edit/:creneauId', {templateUrl: 'assets/pages/partials/admin/submitcreneau.html', controller: EditCreneauController});
+
+    $routeProvider.when('/admin/proposals/list', {templateUrl: '/assets/pages/partials/admin/proposals.html', controller: ListProposalsController});
+    $routeProvider.when('/submitproposal/new', {templateUrl: '/assets/pages/partials/proposal/submit.html', controller: NewProposalController});
+    $routeProvider.when('/editproposal/:proposalId', {templateUrl: '/assets/pages/partials/proposal/submit.html', controller: EditProposalController});
+    $routeProvider.when('/manageproposal', {templateUrl: '/assets/pages/partials/proposal/manage.html', controller: ManageProposalController});
+    $routeProvider.when('/proposals/see/:proposalId', {templateUrl: '/assets/pages/partials/proposal/proposal.html', controller: SeeProposalsController});
 
     $routeProvider.when('/admin/events', {templateUrl: 'assets/pages/partials/event/events.html', controller: EventController});
     $routeProvider.when('/admin/event/new', {templateUrl: 'assets/pages/partials/event/submit.html', controller: NewEventController});
@@ -29,13 +33,11 @@ App.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/admin/track/new', {templateUrl: 'assets/pages/partials/track/submit.html', controller: NewTrackController});
     $routeProvider.when('/admin/track/edit/:id', {templateUrl: 'assets/pages/partials/track/submit.html', controller: EditTrackController});
 
-
     $routeProvider.when('/admin/dynamicfields', {templateUrl: 'assets/pages/partials/admin/dynamicfields.html', controller: DynamicFieldsController});
     $routeProvider.when('/admin/dynamicfield/new', {templateUrl: 'assets/pages/partials/admin/submitdynamicfield.html', controller: NewDynamicFieldController});
     $routeProvider.when('/admin/dynamicfield/edit/:dynamicFieldId', {templateUrl: 'assets/pages/partials/admin/submitdynamicfield.html', controller: EditDynamicFieldController});
     $routeProvider.when('/admin/mailing', {templateUrl: 'assets/pages/partials/admin/mailing.html', controller: MailingController});
 
-    $routeProvider.when('/proposals/see/:proposalId', {templateUrl: '/assets/pages/partials/seeproposal.html', controller: SeeProposalsController});
 
     $routeProvider.when('/profil/:userId', {templateUrl: '/assets/pages/partials/profil.html', controller: ProfilController});
 
